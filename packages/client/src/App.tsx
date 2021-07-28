85690f0b27b4c24c4b95b0e37de0470b55a1e603
Alek Angelov <alek.angelov.sk@gmail.com>
2021-07-28 13:29:50 +0200

    firebase and stuff

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import MainContent from "./components/MainContent";
+import firebase from "firebase";
+import "firebase/auth";
+import { FirebaseAuthProvider } from "@react-firebase/auth";
 import Picker from "./components/Picker";
 import Sidebar from "./components/Sidebar";
 import { TopBar } from "./components/TopBar";
 import { GlobalStyle } from "./styles";
 import { AppContainer, AppContent } from "./styles/Grid";
 import { hexToRgb } from "./utils/hextorgb";
+import Routes from "./components/Routes";
+
+const firebaseConfig = (() => {
+  try {
+    return JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG || "{}");
+  } catch (e) {
+    return {};
+  }
+})();
+console.log(firebaseConfig);
 const colors = {
   light: {
     primary: "#85E6FF",
@@ -21,32 +33,36 @@ const colors = {
 };
 function App() {
   return (
-    <ThemeProvider
-      theme={{
-        colors: colors.light,
-        space: ["0px", "7px", "14px", "24px", "32px", "48px"],
-        fontSize: ["7px", "14px", "24px", "32px", "48px"],
-        borderRadius: 0,
-        shadow: ["0px", "7px", "14px", "24px", "32px", "48px"].map((e) => {
-          return `0 10px ${e} -${parseInt(e) / 2}px rgba(${hexToRgb(
-            colors.light.text,
-            true
-          )}, 0.2)`;
-        }),
-        transition: (...args) =>
-          args.map((arg) => `${arg} 0.2s ease-in-out`).join(","),
-      }}
-    >
-      <AppContainer>
-        <GlobalStyle />
-        <TopBar />
-        <AppContent>
-          <Sidebar />
-          <Picker />
-          <MainContent />
-        </AppContent>
-      </AppContainer>
-    </ThemeProvider>
+    <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+      <ThemeProvider
+        theme={{
+          colors: colors.light,
+          space: ["0px", "7px", "14px", "24px", "32px", "48px"],
+          fontSize: ["7px", "14px", "24px", "32px", "48px"],
+          borderRadius: 0,
+          shadow: ["0px", "7px", "14px", "24px", "32px", "48px"].map((e) => {
+            return `0 10px ${e} -${parseInt(e) / 2}px rgba(${hexToRgb(
+              colors.light.text,
+              true
+            )}, 0.2)`;
+          }),
+          transition: (...args) =>
+            args.map((arg) => `${arg} 0.2s ease-in-out`).join(","),
+        }}
+      >
+        <Routes.Wrapper>
+          <AppContainer>
+            <GlobalStyle />
+            <TopBar />
+            <AppContent>
+              <Sidebar />
+              <Picker />
+              <Routes.Switch />
+            </AppContent>
+          </AppContainer>
+        </Routes.Wrapper>
+      </ThemeProvider>
+    </FirebaseAuthProvider>
   );
 }
 
