32ac82ba370561397aa4fe65bdd64f5d6e342862
Alek Angelov <alek.angelov.sk@gmail.com>
2021-09-01 23:54:42 +0200

    rearranging the arch a bit

--- packages/client/src/components/Routes/pages/Home.tsx
+++ packages/client/src/components/Routes/pages/Home.tsx
@@ -13,30 +13,6 @@ import { useHistory } from "react-router-dom";
 import Tabs from "../../common/Tabs";
 import { hasCharacter, hasTags } from "../../../utils/toolkit";
 
-const OnlineCombos = () => {
-  const { filter, tags } = useAppSelector((state) => state.filters);
-  const fireStore = useFirestore();
-  const combosRef = useMemo(() => {
-    let combos = fireStore.collection("combos");
-    if (tags.length)
-      combos = combos.where(`tags`, `array-contains-any`, tags) as any;
-
-    if (filter) {
-    }
-
-    return combos;
-  }, [fireStore, tags, filter]);
-
-  const combos = useFirestoreCollectionData(combosRef);
-  return (
-    <>
-      {combos?.data?.map((e) => (
-        <SmallCombo {...(e as ICombo)} />
-      ))}
-    </>
-  );
-};
-
 const LocalCombos = () => {
   const { filter, tags } = useAppSelector((state) => state.filters);
   const localCombos = useAppSelector((state) => {
@@ -74,20 +50,9 @@ const Home = () => {
         Home
       </PageHeader>
       <Filters />
-      <Tabs.Wrapper>
-        <Tabs.Pane key="local" title="Local Combos">
-          <PageInner>
-            <LocalCombos />
-          </PageInner>
-        </Tabs.Pane>
-        <Tabs.Pane key="global" title="Global Combos">
-          <PageInner>
-            <RelativeSuspense fixed={false}>
-              <OnlineCombos />
-            </RelativeSuspense>
-          </PageInner>
-        </Tabs.Pane>
-      </Tabs.Wrapper>
+      <PageInner>
+        <LocalCombos />
+      </PageInner>
     </PageWrapper>
   );
 };
